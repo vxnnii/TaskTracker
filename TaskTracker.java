889213508f3bf015739b5ceabf053d20bd31f4ac
vxnni <jovanni.elias@gmail.com>
2026-07-09 22:31:35 -0500

    imports pulled, ArrayLists set and program set to running

--- TaskTracker.java
+++ TaskTracker.java
@@ -12,6 +12,21 @@ Purpose: This program will be used to track users tasks.
 
 I am purposefuly going to put everything under one single file to start and then show how we break it down for us to have a running, loose coupled program. This would be the heart of Object Oriented Programming with multiple classes rather that one huge class. 
 */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class TaskTracker {
 
+  static ArrayList<String> tasks = new ArrayList<>();
+  static ArrayList<Boolean> completed = new ArrayList<>();
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    boolean running = true;
+    System.out.println("=== Student Task Tracker ===");
+}
 }
