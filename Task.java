d9700f7a3f4855d0421e9ce6823816f43d843e0a
vxnni <jovanni.elias@gmail.com>
2026-07-10 15:28:29 -0500

    Task class completed and comment header added.

--- Task.java
+++ Task.java
@@ -1,1 +1,27 @@
-//Task java file
+/* 
+Author: Jovanni Elias
+
+Purpose: This class will handle each task object individually. Each task accepts a description and is paired with either the boolean Done or Pending under the variable completed.
+*/
+
+public class Task {
+
+    private String description;
+    private boolean completed;
+
+    public Task (String description) {
+
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void markComplete() {
+        
+        completed = true;
+    }
+
+    @Override 
+    public String toString() {
+        return description + (completed ? "[Done]" : "[Pending]");
+    }
+}
