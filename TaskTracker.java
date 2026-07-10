75a9676f6e582591b796f1c76e2e47ff02179a23
vxnni <jovanni.elias@gmail.com>
2026-07-09 22:48:24 -0500

    TaskTracker program complete and fully functional

--- TaskTracker.java
+++ TaskTracker.java
@@ -1,8 +1,23 @@
+/*
+Author: Jovanni Elias
+
+Purpose: This program will be used to track users tasks. 
+
+    They will be able to
+    - Add a task
+    - View tasks
+    - Mark task as complete
+    - Remove tasks 
+    - Exit the program
+
+I am purposefuly going to put everything under one single file to start and then show how we break it down for us to have a running, loose coupled program. This would be the heart of Object Oriented Programming with multiple classes rather that one huge class. 
+*/
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskTracker {
-  
+
   static ArrayList<String> tasks = new ArrayList<>();
   static ArrayList<Boolean> completed = new ArrayList<>();
   public static void main(String[] args) {
