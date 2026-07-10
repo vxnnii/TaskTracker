a4c6fb39ed65bfa128e36c0b8b8f4eafeac68e97
vxnni <jovanni.elias@gmail.com>
2026-07-10 15:32:00 -0500

    TaskManager class completed with a comment header

--- TaskManager.java
+++ TaskManager.java
@@ -1,1 +1,24 @@
-//Task Manager java file
+/*
+Author: Jovanni Elias
+
+Purpose: This class will manage a collection of Task objects, allowing for adding, removing, and viewing tasks.
+*/
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    private ArrayList<Task> tasks = new ArrayList<>();
+    
+    public void addTask(String desc) {
+        
+        tasks.add(new Task(desc));
+    }
+
+    public void completeTask(int index) {
+        tasks.get(index).markComplete();
+    }
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
